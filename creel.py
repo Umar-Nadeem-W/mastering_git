61972ae0924db88f6c83b3e53b38c276014ac0b4
Umar-Nadeem-W <umarnadeem.w@gmail.com>
2025-03-29 21:34:49 +0500

    Idk why thing

--- creel.py
+++ creel.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 print("TRBD FCN ICWCH RC"
 "DWEHBWBUBDW"
-=======
-print("he sha ios jC"
-"for it is not a conflict"
->>>>>>> umarlast
 "CWBUOEWDH")
 
 
@@ -12,8 +7,4 @@ print("he sha ios jC"
 print(5*55)
 
 
-<<<<<<< HEAD
-print("This is from the main version of creel")
-=======
 print("we call them the flares")
->>>>>>> umarlast
