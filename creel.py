d88236805c58c560367e9736fd31c7f9d11c61b3
Umar-Nadeem-W <umarnadeem.w@gmail.com>
2025-03-29 21:14:00 +0500

    changed creel up

--- creel.py
+++ creel.py
@@ -1,7 +1,4 @@
-print("This line is common to both")
-
-print("this Line1 is from the cryin side")
-
-print("this Line2 is from the cryin side")
-
-print("this also")
+print("TRBD FCN ICWCH RC"
+"DWEHBWBUBDW"
+"CWBUOEWDH")
+print(5*55)
